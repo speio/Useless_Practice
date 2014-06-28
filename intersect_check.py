051a2c21211fabfc97122cd3a9b6195c3583ae04
Geno V <g.villafano@gmail.com>
2014-06-28 07:28:06 +0200

    added error checks and have parts of it running

--- intersect_check.py
+++ intersect_check.py
@@ -3,7 +3,7 @@
 #want to know if they want to eat peanut butter or oreos, not eat any
 # eat just the peanut butter, eat just the oreos, or eat both. 
 
-def string_intersect(input):
+def intersect_check(input):
 	
 ##Lists for checking against
 	O = ["eat", "it"]
@@ -13,44 +13,69 @@ def string_intersect(input):
 	d = ["don't", "do not", "not"] # negatives 
 	e = ["or", "nor"] # joining
 
-
+	decision = "none"
 
 ##Checking intersections
-	o_o = list(set(O)) & set(input.split())
-	a_a = list(set(a) & set(input.split()))
-	b_b = list(set(b) & set(input.split()))
-	c_c = list(set(c) & set(input.split()))
-	d_d = list(set(d) & set(input.split()))
+	o_o = list((set(O)) & set(input.split()))
+	a_a = list((set(a)) & set(input.split()))
+	b_b = list((set(b)) & set(input.split()))
+	c_c = list((set(c)) & set(input.split()))
+	d_d = list((set(d)) & set(input.split()))
 	e_e = list(set(e) & set(input.split()))
 	
 ##Using combinations of intersections to discern meaning
+	while decision == "none" or "confused":
+		print "O %s" % o_o
+		print "a %s" % a_a
+		print "b %s" % b_b
+		print "c %s" % c_c
+		print "d %s" % d_d
+		print "e %s" % e_e 
+		input = str(raw_input("(internal)input response to check \n > "))
+		
+		o_o = list((set(O)) & set(input.split()))
+		a_a = list((set(a)) & set(input.split()))
+		b_b = list((set(b)) & set(input.split()))
+		c_c = list((set(c)) & set(input.split()))
+		d_d = list((set(d)) & set(input.split()))
+		e_e = list(set(e) & set(input.split()))
 
-	#If no specifics about which to eat are given
-	if len(o_o) >= 1 and len(d_d) = 0 and len(c_c) = 0:
-		print "Eat what? The box? Let's not talk like apes"
-		#ask for input again
-		decision = "confused"
+
+		#If no specifics about which to eat are given
+		if len(o_o) >= 1 and len(d_d) == 0 and len(c_c) == 0:
+			print "Eat what? The box? Let's not talk like apes"
+			#ask for input again
+			decision = "confused"
+			print decision
 		
-	#Trying to figure out what to do if both oreo and peanut included in input
-	elif len(c_c) > len(b_b) and len(c_c) > len(a_a): #if 
-		print "included peanut and oreo in input"
+		#Trying to figure out what to do if both oreo and peanut included in input
+		elif len(c_c) > len(b_b) and len(c_c) > len(a_a): #if 
+			print "included peanut and oreo in input"
 		
-		#list of negatives not empty but two weren't used and no joining words used
-		#implying input is using both items but wants opposite actions attached to each
-		if len(d_d) != 0 and len(d_d) < 2 and len(e_e) == 0:
-			print "eating only one of them"
-			#if distance between peanut or peanut butter and *NEGATIVE*	in input.split() greater
-			#than distance between oreos and negative than: print "eating peanut butter"
-			# but not eating oreos. else: eating peanut butter and not eating oreos.
+			#list of negatives not empty but two weren't used and no joining words used
+			#implying input is using both items but wants opposite actions attached to each
+			if len(d_d) != 0 and len(d_d) < 2 and len(e_e) == 0:
+				print "eating only one of them"
+				#if distance between peanut or peanut butter and *NEGATIVE*	in input.split() greater
+				#than distance between oreos and negative than: print "eating peanut butter"
+				# but not eating oreos. else: eating peanut butter and not eating oreos.
 		
-		elif len(d_d) == 1 and len(e_e) != 0:
-			print "Not eating either"
-			decision = "neither"
+			elif len(d_d) == 1 and len(e_e) != 0:
+				print "Not eating either"
+				decision = "neither"
 		
-		#This probably won't be reached unless trying to input something confusing
-		elif len(d_d) >= 2:
-			print "That's a confusing way to say you are not eating either of them"
-			decision = "neither"
+			#This probably won't be reached unless trying to input something confusing
+			elif len(d_d) >= 2:
+				print "That's a confusing way to say you are not eating either of them"
+				decision = "neither"
+			else:
+				print "eating both"
+				decision = "both"
+				print decision	
+		
 		else:
-			print "eating both"
-			decision = "both"
+			print "Not sure what that means, give it another go"
+			decision = "none"
+
+user_input = str(raw_input("input response to check \n > "))
+intersect_check(user_input)			

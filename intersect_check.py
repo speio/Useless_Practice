210c42378925d5e121fcebc427f2d49947937d25
Geno V <g.villafano@gmail.com>
2014-06-28 08:44:32 +0200

    seems to work properly and predict what is to be said, all error checks still left in

--- intersect_check.py
+++ intersect_check.py
@@ -24,7 +24,7 @@ def intersect_check(input):
 	e_e = list(set(e) & set(input.split()))
 	
 ##Using combinations of intersections to discern meaning
-	while decision == "none" or "confused":
+	while decision == "none":
 		print "O %s" % o_o
 		print "a %s" % a_a
 		print "b %s" % b_b
@@ -47,8 +47,15 @@ def intersect_check(input):
 			#ask for input again
 			decision = "confused"
 			print decision
-		
-		#Trying to figure out what to do if both oreo and peanut included in input
+			
+		#If neither food is to be eaten
+		elif len(o_o) >= 1 and len(d_d) >= 1:
+			print "I guess you're not eating either?"
+			decision = "not either"
+			print decision	
+			
+			
+		#Trying to figure out what to do if both oreo and peanut included in input		
 		elif len(c_c) > len(b_b) and len(c_c) > len(a_a): #if 
 			print "included peanut and oreo in input"
 		
@@ -56,9 +63,75 @@ def intersect_check(input):
 			#implying input is using both items but wants opposite actions attached to each
 			if len(d_d) != 0 and len(d_d) < 2 and len(e_e) == 0:
 				print "eating only one of them"
-				#if distance between peanut or peanut butter and *NEGATIVE*	in input.split() greater
-				#than distance between oreos and negative than: print "eating peanut butter"
-				# but not eating oreos. else: eating peanut butter and not eating oreos.
+				input_list = list(input.split())
+				
+				#finding the index of the negative
+				index = 0
+				found = "no"
+				while found != "yes":
+					index = index + 1
+					print index
+					if input_list[index] in d:
+						print "found index of negative"
+						i_negative = index
+						found = "yes"
+						 
+					else:
+						print "not found yet"
+						
+				#finding index of peanut 
+				index = 0
+				found = "no"
+				while found != "yes":
+					index = index + 1
+					print index
+					if input_list[index] in a:
+						print "found index of peanut"
+						i_peanut = index
+						
+						#getting distance between peanut and negative
+						dist_peanut = i_peanut - i_negative
+						found = "yes"
+						 
+					else:
+						print "not found yet"
+				
+				#finding index of oreos
+				index = 0
+				found = "no"
+				while found != "yes":
+					index = index + 1
+					print index
+					if input_list[index] in b:
+						print "found index of oreos"
+						i_oreos = index
+						
+						#getting distance between oreos and negative
+						dist_oreos = i_oreos - i_negative
+						found = "yes"
+						 
+					else:
+						print "not found yet"	
+				
+				#negative before object assumption
+				if i_negative < i_peanut and i_negative < i_oreos:
+					
+					if dist_peanut > dist_oreos:
+						print "eating peanut butter"
+				
+					else:
+						print "eating oreos"
+							
+						
+				
+				#negative after object assumption
+				else:
+					if dist_peanut > dist_oreos:
+						print "eating oreos"
+					else:
+						print "eating peanut butter"	
+					
+
 		
 			elif len(d_d) == 1 and len(e_e) != 0:
 				print "Not eating either"
@@ -72,6 +145,13 @@ def intersect_check(input):
 				print "eating both"
 				decision = "both"
 				print decision	
+		
+		#Eating just the penut butter (no mention oreos)
+		elif len(o_o) >= 1 and len(a_a) >= 1 and len(b_b) == 0:
+			print "eating just the peanut butter \n no mention of oreos"
+		
+		elif len(o_o) >= 1 and len(b_b) >= 1 and len(a_a) == 0:
+			print "eating just oreos, no mention of peanut butter"
 		
 		else:
 			print "Not sure what that means, give it another go"
